9574f074ca2b975eabbdbd37193b64e0c945971d
Jose Pablo Fuentes <38989329+Apu1398@users.noreply.github.com>
2023-01-31 21:11:40 -0600

    Ejercicio 1

---
+++ UniversityApiBackend/Models/DataModels/Course.cs
@@ -0,0 +1,36 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class Course: BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+
+        [Required, StringLength(180)]
+        public string ShortDescription { get; set; } = string.Empty;
+
+        [Required]
+        public string Largedescription { get; set; } = string.Empty;
+
+        [Required]
+        public string TargetAudience { get; set; } = string.Empty;
+
+        [Required]
+        public string Objectives { get; set; } = string.Empty;
+
+        [Required]
+        public string Requirements { get; set; } = string.Empty;
+
+        [Required]
+        public Levels Level { get; set; } = Levels.Basic;
+
+
+        public enum Levels
+        {
+            Basic, Intermediate, Advanced
+        } 
+
+    }
+}
---
+++ UniversityApiBackend/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.DataAccess;
+
+var builder = WebApplication.CreateBuilder(args);
+
+//Connect to SQL 
+const string CONNECTIONNAME = "UniversityDB";
+var connectionString  = builder.Configuration.GetConnectionString(CONNECTIONNAME);
+
+
+//Add context
+
+builder.Services.AddDbContext<UniversityDBContext>(optionsAction: options => options.UseSqlServer(connectionString));
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
